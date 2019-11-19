7d63565accc24a7361fffd7f6d96059355224f51
Bjorn Melin <melinb@augsburg.edu>
2019-11-19 09:56:58 -0600

    Added Comments For Test Functions

--- test/test.js
+++ test/test.js
@@ -13,36 +13,69 @@ describe('Log into ShopStream', function () {
     before(async function() {
         driver = await new Builder().forBrowser('chrome').build();
     });
-    it('Login To Shopstream', async function() {
+
+    /**
+     * Load the ShopStream Login Page.
+     * Loads 'http://localhost:3000/#' stored in serverUri
+     */
+    it('Load The ShopStream Login Page', async function() {
         // Load the page
         await driver.get(serverUri);
     });
 
+    /**
+     * Enter Email in Login page.
+     */
     it('Enter Email', async function() {
         // enter email
         driver.findElement(By.id('email')).sendKeys('Email Address');
     });
 
+    /**
+     * Enter Password in Login page.
+     */
     it('Enter password', async function() {
         // enter password
         driver.findElement(By.id('password')).sendKeys('Password');
     });
 
-    it('Submit Form', async function() {
-        // submit form
+    /**
+     * Click Login Button on Login page.
+     * Submits the Login form.
+     */
+    it('Submit Login Form', async function() {
+        // submit login form
         driver.findElement(By.id('signin')).click();
     });
 
+
+    /**
+     * After logged in, change route to the apps home dashboard.
+     * Click the main menu drawer button to test that it opens.
+     */
     it('Open App Bar Drawer', async function() {
+      // route to apps home dashboard
       await driver.get("http://localhost:3000/dashboard");
+      // open main menu drawer on left side of dashboard
       driver.findElement(By.id('openMenuDrawer')).click();
     });
 
+
+    /**
+     * Click the main menu drawer arrow to close the main menu drawer.
+     */
     it('Close App Bar Drawer', async function() {
+      // close main menu drawer on left side of dashboard 
       driver.findElement(By.id('closeMenuDrawer')).click();
     });
 
+
+    /**
+     * Click the logout button in top right of dashboard.
+     * Should route a user back to the login screen.
+     */
     it('Logout', async function() {
+      // click logout
       driver.findElement(By.id('logout')).click();
     });
 
